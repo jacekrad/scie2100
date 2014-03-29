d2c73f4b3c5519c11395f267a9ac38e9292527f7
Jacek Radajewski <jacekrad@gmail.com>
2014-03-29 17:31:11 +1000

    retrieved data in ex6 and saved to files

--- prac01/exercise6.py
+++ prac01/exercise6.py
@@ -10,15 +10,18 @@ import os.path as path
 q5_filename = "sigpep_at.fa"
 q6_filename = "lipmet_at.fa"
 
-""" for both questions 5 and 6 we check if the fasta file already exists and only do the searches and obtain sequences if false 
-    this is we can rerun the program quickly without rebuilding the fasta file each and every time
+""" for both questions 5 and 6 we check if the fasta file already exists 
+    and only do the searches and obtain sequences if false 
+    this is we can rerun the program quickly without rebuilding the 
+    fasta file each and every time
 """
 
 """ QUESTION 5 """
 if not(path.isfile(q5_filename)):
     q5_sequences = []
     """ set of ids returned as part of the protein search in question 5 """
-    q5_ids = seq.searchSequences("signal+peptide+AND+organism:Arabidopsis+thaliana[3702]+AND+length:[100+TO+*]")
+    q5_ids = seq.searchSequences("signal+peptide+AND+organism:Arabidopsis+thaliana[3702]+" 
+                                 + "AND+length:[100+TO+*]")
     print "Processing ", len(q5_ids), " sequences ..."
     for seq_id in q5_ids:
         print "Fetching sequence: ", seq_id
@@ -32,7 +35,8 @@ else:
 if not(path.isfile(q6_filename)):
     q6_sequences = []
     """ set of ids returned as part of the question 6 search  """ 
-    q6_ids = seq.searchSequences("Lipid+metabolism+AND+organism:3702+AND+fragment:no+AND+length:[100+TO+*]")
+    q6_ids = seq.searchSequences("Lipid+metabolism+AND+organism:3702+AND+fragment:no+"
+                                 + "AND+length:[100+TO+*]")
     print "Processing ", len(q6_ids), " sequences ..."
     for seq_id in q6_ids:
         print "Fetching sequence: ", seq_id
